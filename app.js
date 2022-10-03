c2668c9e4dcd7f1875bbb36fbc9e7015157112e9
vuxpham <phamanhvu281003@gmail.com>
2022-10-03 11:45:36 +0700

    Nice

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const http = require('http'); //use http module
 
 const server = http.createServer((req, res)=>{    //create server object with a function as parameter. Function takes in 
 												  //2 parameters, request and response
-	console.log(req);
+	console.log(req.url, req.method, req.headers);
+	
+	res.setHeader('Content-Type', 'text/html');   //res = response. Response with setting the header with type text/html 
+	res.write('<html>');
+	res.write('<head><title>Page Title</title></head>'); //write response into an html file that will return when user request access to site
+	res.write('<body><h1>Hello</h1></body>');
+	res.write('</html>');
+	res.end();                                           //response ends here, write more after = error
 	
 });
 
