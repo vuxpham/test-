dc629c05f4fe71c8bba0c659b6552b818967aeee
vuxpham <phamanhvu281003@gmail.com>
2022-10-03 12:02:06 +0700

    Routing Requests

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const http = require('http'); //use http module
 
 const server = http.createServer((req, res)=>{    //create server object with a function as parameter. Function takes in 
 												  //2 parameters, request and response
-	console.log(req.url, req.method, req.headers);
 	
-	res.setHeader('Content-Type', 'text/html');   //res = response. Response with setting the header with type text/html 
+	const url = req.url;
+	
+	if(url === '/'){                              //if input url after host is exactly "/", return this html page
+		res.write('<html>');
+		res.write('<head><title>Enter things</title></head>'); //write response into an html file that will return when user request access to site
+		res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+		res.write('</html>');
+		return res.end();                         //return so that it exits the function, and the write after does not run after the end method, causing error
+	}
+	
+	res.setHeader('Content-Type', 'text/html');   //after form submitted, url changed to /message => new file => this code runs without error
 	res.write('<html>');
-	res.write('<head><title>Page Title</title></head>'); //write response into an html file that will return when user request access to site
+	res.write('<head><title>Page Title</title></head>'); 
 	res.write('<body><h1>Hello</h1></body>');
 	res.write('</html>');
 	res.end();                                           //response ends here, write more after = error
