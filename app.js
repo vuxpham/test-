d307ff5ca2296c90d06caad8825d9838e55581b3
vuxpham <phamanhvu281003@gmail.com>
2022-10-04 11:14:48 +0700

    Separated request handling code into another file

--- app.js
+++ app.js
@@ -1,47 +1,7 @@
 const http = require('http'); //use http module
-const fs = require('fs');
 
-const server = http.createServer((req, res)=>{    
-	
-	const url = req.url;
-	const method = req.method;
-	
-	if(url === '/'){                              
-		res.write('<html>');
-		res.write('<head><title>Enter things</title></head>'); 
-		res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-		res.write('</html>');
-		return res.end();                         
-	}
-	if(url === '/message' && method === 'POST')
-	{
-		const a = 0;
-		const body = [];                          
-		req.on('data', chunk => {				   
-			console.log(chunk);                   
-			console.log(body.push(chunk));
-		});
-		
-		req.on('end', ()=>{                                          
-			const parsedBody = Buffer.concat(body).toString();       
-			console.log(parsedBody);                                 
-			const message = parsedBody.split('=')[1];                 
-			fs.writeFileSync('message.txt', message);                
-		});												              
-	
-		res.statusCode = 302;                     
-		res.setHeader('Location', '/');
-		return res.end();
-		
-	}
-	
-	res.setHeader('Content-Type', 'text/html');   
-	res.write('<html>');
-	res.write('<head><title>Page Title</title></head>'); 
-	res.write('<body><h1>Hello</h1></body>');
-	res.write('</html>');
-	res.end();                                         
-	
-});
+const routes = require('./routes');   //import custom module
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);                                  
